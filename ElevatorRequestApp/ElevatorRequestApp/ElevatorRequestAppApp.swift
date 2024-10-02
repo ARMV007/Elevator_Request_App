fd92700058b45487abe185c51236d52c131444dc
Abhishek Verma <abhishek.rmv@gmail.com>
2024-10-02 15:49:51 +0530

    Initial setup
    Add base project

---
+++ ElevatorRequestApp/ElevatorRequestApp/ElevatorRequestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorRequestAppApp.swift
+//  ElevatorRequestApp
+//
+//  Created by Abhishek on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorRequestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
